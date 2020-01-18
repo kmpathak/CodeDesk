cd8f614a9b7cbe07dc383b67a5f7ed5b86bf2b33
kmpathak <murtikrishna58@gmail.com>
2020-01-18 23:21:47 +0530

    restfull api first update

---
+++ smartbookinglookup/src/test/java/com/webscrapper/smartbookinglookup/SmartbookinglookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webscrapper.smartbookinglookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbookinglookupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
