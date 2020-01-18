c294cef99948fbb60f63a31cb6e220c0d8c133b8
kmpathak <murtikrishna58@gmail.com>
2020-01-18 23:57:38 +0530

    Restfull Api initial built

---
+++ smartbookinglookup/src/main/java/com/webscrapper/smartbookinglookup/SmartbookinglookupApplication.java
@@ -0,0 +1,13 @@
+package com.webscrapper.smartbookinglookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbookinglookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbookinglookupApplication.class, args);
+	}
+
+}
