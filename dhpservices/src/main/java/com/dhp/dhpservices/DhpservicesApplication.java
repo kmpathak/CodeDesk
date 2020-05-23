381c0b8de305ee7e3e544bd5c206ac28ea86d1bf
kmpathak <murtikrishna58@gmail.com>
2020-05-23 21:31:20 +0530

    adding first "dhp" web service for all dhp ortal application

---
+++ dhpservices/src/main/java/com/dhp/dhpservices/DhpservicesApplication.java
@@ -0,0 +1,13 @@
+package com.dhp.dhpservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhpservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhpservicesApplication.class, args);
+	}
+
+}
---
+++ dhpservices/src/test/java/com/dhp/dhpservices/DhpservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhp.dhpservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhpservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
